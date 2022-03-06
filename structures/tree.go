daafd1a6925cae2d99eb26b7945f8638ea5a0b56
halafi <filiphalas74@gmail.com>
2022-03-06 15:40:23 -0500

    feat: tree

---
+++ structures/tree.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Left  *TreeNode
+	Right *TreeNode
+	Value int
+}
+
+var DefaultValue int = -1024
+
+func InsertNodeToTree(tree *TreeNode, node *TreeNode) {
+	if tree == nil {
+		return
+	}
+	if tree.Value == DefaultValue {
+		tree.Value = node.Value
+		return
+	}
+	if node.Value > tree.Value {
+		if tree.Right == nil {
+			tree.Right = &TreeNode{Value: DefaultValue}
+		}
+		InsertNodeToTree(tree.Right, node)
+	}
+	if node.Value < tree.Value {
+		if tree.Left == nil {
+			tree.Left = &TreeNode{Value: DefaultValue}
+		}
+		InsertNodeToTree(tree.Left, node)
+	}
+}
+
+func InitTree(values ...int) (root *TreeNode) {
+	rootNode := TreeNode{Value: DefaultValue, Right: nil, Left: nil}
+	for _, value := range values {
+		fmt.Printf("%d", value)
+		node := TreeNode{Value: value}
+		fmt.Println("insert node to tree")
+		InsertNodeToTree(&rootNode, &node)
+	}
+	return &rootNode
+}
+
+func main() {
+	tree := InitTree(1, 2, 3, 4, 5, 0)
+	fmt.Println(tree.Value)
+	fmt.Println(tree.Left)
+	fmt.Println(tree.Right)
+	fmt.Println(tree.Right.Value)
+	fmt.Println(tree.Right.Left)
+	fmt.Println(tree.Right.Right)
+
+}
